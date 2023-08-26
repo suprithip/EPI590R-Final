99054f81d97dc579b6ace0f07b317acab9d25a94
Suprithi Pingle <suprithi.pingle@gmail.com>
2023-08-26 17:23:18 -0400

    Functions

--- Final.R
+++ Final.R
@@ -5,12 +5,14 @@ gitcreds::gitcreds_set()
 install.packages("here")
 load(file='opt.rda')
 here::here()
+#descriptive statistics
 library(gtsummary)
 tbl_summary(
 	opt,
 	by=Clinic,
 	include=c(Group, Age, Black, White, Nat.Am, Asian, Hisp, Education
 	))
+#regression model
 logistic_model<-glm(Group~Age+Education, data=opt, family = binomial())
 tbl_regression(
 	logistic_model,
@@ -19,9 +21,17 @@ tbl_regression(
 		Age ~ "Age of Women",
 		Education ~ "Level of Education"
 	))
+#regression model w/broom
 library(broom)
 glance(logistic_model, conf.int=TRUE, exponentiate=TRUE)
+#figure
 boxplot(opt$Age~opt$Clinic,
 				xlab="Clinic",
 				ylab="Age",
 				main="Participants Ages by Clinic")
+#function: Calculate Average Qualifying Teeth Among Those Who Received Treatment
+city<-function(x,y,multiplier=100){
+new_val=multiplier*sum(x)/sum(y)
+return(new_val)
+}
+city(opt$Clinic=="NY",opt$Group=="T")
